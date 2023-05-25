ec604c2737e3325908fd7e6ffa6f4387edd1fa38
Raiy <raiyaries@gmail.com>
2023-05-25 12:02:04 +1000

    Site updated: 2023-05-25 12:02:04

--- books/js/events.js
+++ books/js/events.js
@@ -13,27 +13,33 @@ Fluid.events = {
       tag.addEventListener('click', function(event) {
         var tagName = this.getAttribute('data-name'); // 获取被点击的标签的名称
         var searchVal = document.querySelector('#search-input'); // 获取搜索框元素
-        searchVal.value = '#' + tagName; // 将搜索框的值设置为 '#' + 标签的名称
-        var searchTagsName = ('#'+ tagName).toLowerCase();
-        
-        $('.index-card').each(function() {
-          var match = false;
-          $(this).find('.post-meta a').each(function() {
-            var tagName = $(this).text().toLowerCase(); //获取标签名称
-            if (tagName === searchTagsName) {
-              match = true;
-              return false; // 中断 each 循环
+
+        if (tagName === '#All'){
+          searchVal.value = ''; // 将搜索框的值设置为 '#' + 标签的名称
+          var searchTagsName = '';
+        } else {
+          searchVal.value = '#' + tagName; // 将搜索框的值设置为 '#' + 标签的名称
+          var searchTagsName = ('#'+ tagName).toLowerCase();
+          $('.index-card').each(function() {
+            var match = false;
+            $(this).find('.post-meta a').each(function() {
+              var tagName = $(this).text().toLowerCase(); //获取标签名称
+              if (tagName === searchTagsName) {
+                match = true;
+                return false; // 中断 each 循环
+              }
+            });
+            
+            // 如果匹配成功，显示该卡片，否则隐藏
+            if (match) {
+              $(this).show();
+              allHidden = false; //至少有一个卡片被显示
+            } else {
+              $(this).hide();
             }
           });
-          
-          // 如果匹配成功，显示该卡片，否则隐藏
-          if (match) {
-            $(this).show();
-            allHidden = false; //至少有一个卡片被显示
-          } else {
-            $(this).hide();
-          }
-        });
+        }
+
         // 如果所有卡片都被隐藏，显示“没有相应结果”，否则隐藏
         if (allHidden) {
           $('#no-results').show();
