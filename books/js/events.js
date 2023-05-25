0747e560203a64519adf1de7bdb388d3b493243d
Raiy <raiyaries@gmail.com>
2023-05-25 12:30:24 +1000

    Site updated: 2023-05-25 12:30:24

--- books/js/events.js
+++ books/js/events.js
@@ -18,6 +18,8 @@ Fluid.events = {
         if (tagName === 'all'){
           searchVal.value = ''; // 将搜索框的值设置为 '#' + 标签的名称
           var searchTagsName = '';
+          $(this).show();
+          allHidden = false; 
         } else {
           searchVal.value = '#' + tagName; // 将搜索框的值设置为 '#' + 标签的名称
           var searchTagsName = ('#'+ tagName).toLowerCase();
